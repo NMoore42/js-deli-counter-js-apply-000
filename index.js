804986a2aeb6717f0a15204699bc097021267d20
organized-quotation-1910 <nicholas.moore5@icloud.com>
2019-01-14 23:20:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function takeANumber(katzDeliLine = [], custName){
 function nowServing(katzDeliLine){
   //return the first person in line and remove individual in line
   if (katzDeliLine.length > 1){
-    return(katzDeliLine[0].pop())
+    return(katzDeliLine[0].shift())
   } else {
-    
+    return "There is nobody waiting to be served!"
   }
   //If no more people are in line, return "There is nobody waiting to be served"
 }
