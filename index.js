6f9751183fdf3a5c0800231acefa1a55b6aa10ef
organized-quotation-1910 <nicholas.moore5@icloud.com>
2019-01-14 22:52:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function takeANumber(katzDeliLine, custName){
+  //return welcome message including person's name and place in line (index +1)
+  
+}
+
+function nowServing(katzDeliLine){
+  //return the first person in line and remove individual in line
+  //If no more people are in line, return "There is nobody waiting to be served"
+}
