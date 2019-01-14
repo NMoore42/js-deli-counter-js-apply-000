02896d4db9ea5efd72af9ab2bb57956a7adb655d
organized-quotation-1910 <nicholas.moore5@icloud.com>
2019-01-14 22:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function nowServing(katzDeliLine){
   //return the first person in line and remove individual in line
   //If no more people are in line, return "There is nobody waiting to be served"
 }
+
+function currentLine(katzDeliLine) {
+  //Accepts current line and returns in string format: "The line is currently: Person1, Person 2, ect..."
+  //If nobody is in line return "The line is currently empty."
+}
